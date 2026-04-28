e2578eabaee3a435b56189f40c93b694f1603084
Ever462 <everlopruiz1907@gmail.com>
2026-04-29 00:21:13 +0200

    Update Principal

--- src/main/java/gui/PrincipalGUI.java
+++ src/main/java/gui/PrincipalGUI.java
@@ -2,67 +2,30 @@ package gui;
 
 import java.awt.EventQueue;
 import java.awt.Image;
-
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.SwingConstants;
 import javax.swing.Timer;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
+import java.util.Locale;
 import java.util.ResourceBundle;
 import java.awt.event.ActionEvent;
-import java.awt.CardLayout;
-import javax.swing.Box;
 import javax.swing.ImageIcon;
-
-import java.awt.Panel;
 import javax.swing.JComboBox;
 
 public class PrincipalGUI extends JFrame {
 
-	private static final long serialVersionUID = 1L;
-	private JPanel contentPane;	
-	public PrincipalGUI() {
-		setTitle("Market");
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
-		contentPane.setLayout(null);
-		
-		JButton bGuest = new JButton();
-		bGuest.setBounds(59, 210, 315, 29);
-		bGuest.setText(ResourceBundle.getBundle("Etiquetas").getString("Principal.Guest"));
-		contentPane.add(bGuest);
-		
-		JButton bLogin = new JButton();
-		bLogin.setBounds(126, 118, 178, 29);
-		contentPane.add(bLogin);
-		bLogin.setText(ResourceBundle.getBundle("Etiquetas").getString("MainGUI.Login"));
-		
-		JButton bRegistrar = new JButton(ResourceBundle.getBundle("Etiquetas").getString("MainGUI.Register"));
-		bRegistrar.setBounds(126, 153, 178, 29);
-		contentPane.add(bRegistrar);
-		
-		//Insercción de imagen
-		ImageIcon im = new ImageIcon("icon.png");
-		Image escalada = im.getImage().getScaledInstance(75,75,Image.SCALE_SMOOTH);
-		ImageIcon icon = new ImageIcon(escalada);
-		JLabel panel = new JLabel(icon);
-		panel.setBounds(174, 32, 75, 75);
-		contentPane.add(panel);
-		
-		//Lista del idioma
-		String[] idi = {"es","eus","en"};
-		JComboBox Idioma = new JComboBox(idi);
-		Idioma.setToolTipText(ResourceBundle.getBundle("Etiquetas").getString("PrincipalGUI.Idioma")); 
-		Idioma.setBounds(369, 11, 55, 20);
-		contentPane.add(Idioma);
-		
+    private static final long serialVersionUID = 1L;
+    private JPanel contentPane;    
+    
+    private JButton bGuest;
+    private JButton bLogin;
+    private JButton bRegistrar;
+    private JComboBox<String> Idioma;
 
+<<<<<<< Updated upstream
 		
 		
 		//Abrir ventana de inicio
@@ -101,3 +64,102 @@ public class PrincipalGUI extends JFrame {
 			
 	}
 }
+=======
+    public PrincipalGUI() {
+        setTitle("Market");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBounds(100, 100, 450, 300);
+        contentPane = new JPanel();
+        contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+        setContentPane(contentPane);
+        contentPane.setLayout(null);
+        
+        bGuest = new JButton();
+        bGuest.setBounds(59, 210, 315, 29);
+        contentPane.add(bGuest);
+        
+        bLogin = new JButton();
+        bLogin.setBounds(126, 118, 178, 29);
+        contentPane.add(bLogin);
+        
+        bRegistrar = new JButton();
+        bRegistrar.setBounds(126, 153, 178, 29);
+        contentPane.add(bRegistrar);
+        
+        // Inserción de imagen
+        ImageIcon im = new ImageIcon("icon.png");
+        Image escalada = im.getImage().getScaledInstance(75,75,Image.SCALE_SMOOTH);
+        ImageIcon icon = new ImageIcon(escalada);
+        JLabel panel = new JLabel(icon);
+        panel.setBounds(174, 32, 75, 75);
+        contentPane.add(panel);
+        
+        // Lista del idioma
+        String[] idi = {"es","eus","en"};
+        Idioma = new JComboBox<>(idi);
+        Idioma.setBounds(369, 11, 55, 20);
+        contentPane.add(Idioma);
+        
+        // Cargamos los textos por primera vez
+        paintAgain();
+        
+        // Abrir ventana de inicio
+        bLogin.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                LoginGUI ventanaLogin = new LoginGUI();
+                ventanaLogin.setVisible(true);
+                Timer timer = new Timer(1000, evt -> dispose());
+                timer.setRepeats(false);
+                timer.start();
+            }
+        });
+        
+        // Invocación de la ventana de Registro
+        bRegistrar.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                RegisterGUI ventanaRegister = new RegisterGUI();
+                ventanaRegister.setVisible(true); 
+                Timer timer = new Timer(1000, evt -> dispose());
+                timer.setRepeats(false);
+                timer.start();
+            }
+        });
+        
+        // Modo Invitado
+        bGuest.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                MainGUI ventanaMain = new MainGUI();
+                ventanaMain.setVisible(true);
+                ventanaMain.bLogin.setVisible(true);
+                ventanaMain.bRegist.setVisible(true);
+                Timer timer = new Timer(1000, evt -> dispose());
+                timer.setRepeats(false);
+                timer.start();
+            }
+        });
+        
+        // Cambio de idioma
+        Idioma.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                if(Idioma.getSelectedIndex() == 0) {
+                    Locale.setDefault(new Locale("es"));
+                } else if(Idioma.getSelectedIndex() == 1) {
+                    Locale.setDefault(new Locale("eus"));
+                } else if(Idioma.getSelectedIndex() == 2) {
+                    Locale.setDefault(new Locale("en"));
+                }
+                paintAgain(); 
+            }
+        });
+    }
+
+    public void paintAgain() {
+        ResourceBundle bundle = ResourceBundle.getBundle("Etiquetas");
+        
+        bGuest.setText(bundle.getString("Principal.Guest"));
+        bLogin.setText(bundle.getString("MainGUI.Login"));
+        bRegistrar.setText(bundle.getString("MainGUI.Register"));
+        Idioma.setToolTipText(bundle.getString("PrincipalGUI.Idioma"));
+    }
+}
+>>>>>>> Stashed changes
